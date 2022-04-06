22607738184c9bdd95210bd5cdb17bbbdff4b9d7
VLesieux <vincent.lesieux@ac-lille.fr>
2022-04-06 13:50:48 +0200

    6.4.22

--- Projet_8_Jeux_à_deux_joueurs/jeu_tic_tac.py
+++ Projet_8_Jeux_à_deux_joueurs/jeu_tic_tac.py
@@ -1,92 +1,51 @@
-"""
-
-Jeu Tic tac (morpion)
-
-module : jeu_tic_tac.py
-
-"""
-
-def situation_init(): # initialisation du jeu
+def situation_init():
     """
-    # A completer#
     """
-   # A completer#
-OK   
-    return # A completer#
 
-def action_joueur(valeur_joueur,param_jeu): # choix du joueur
+def test_ligne(param_jeu):
     """
-    # A completer#
+    """
 
+def test_diagonale1(param_jeu):
+    """
+    """
+
+def test_diagonale2(param_jeu):
+    """
     """
     
-# A completer#
-    return # A completer#
+def test_colonne(param_jeu):
+    """
+    """
+
+def test_jeu_rempli(param_jeu):
+    """
+    """
+
+def action_joueur(valeur_joueur,param_jeu):
+    """
+    """
+
+def choix_joueur(valeur_joueur,param_jeu):
+    """
+    """
+
+def test_validite_choix(valeur_joueur,lechoix,param_jeu):
+    """
+    """
 
 def evolution_jeu(valeur_joueur,param_jeu,choix_joueur):
     """
-# A completer#
-
     """
-    
-# A completer#
-    return # A completer#
 
 def aff_evolution_jeu(param_jeu):
     """
-# A completer#
-
     """
-    
-# A completer#
-    return # A completer#
 
 def etat_final(param_jeu):
     """
-# A completer#
+    """
 
-    """
-    
-# A completer#
-
-
-def test_ligne(# A completer#):
-    """
-# A completer#
-OK
-    """
-# A completer#
-
-def test_colonne(# A completer#):
-    """
-# A completer#
-    """
-OK        
-# A completer#
-
-def test_diagonale1(# A completer#):
-    """
-# A completer#
-OK
-    """
-# A completer#
-        return False
-
-def test_diagonale2(# A completer#):
-    """
-# A completer#
-OK
-    """
-# A completer#
-
-def test_plateau_jeu_remplie(# A completer#):
-    """
-# A completer#
-
-    """
-# A completer#
-
-
-if __name__ == '__main__': # ne fait les doctests que si lancé en programme principal
+if __name__ == '__main__':
     import doctest
-    doctest.testmod(verbose=True) # True : affiche tjrs les résultats des tests, False : n'affiche que les erreurs
+    doctest.testmod(verbose=True)
